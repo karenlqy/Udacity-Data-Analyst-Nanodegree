a88a4ee160cd5fa75f13acb97993a9f81bf0c4e2
karenlqy <qingyuli422@gmail.com>
2017-06-06 16:44:38 -0500

    Add files via upload

--- P5-Machine Learning with Enron Dataset/poi_id.py
+++ P5-Machine Learning with Enron Dataset/poi_id.py
@@ -5,12 +5,13 @@ import pickle
 import numpy
 import pandas
 import matplotlib
+from copy import copy
 
-from ggplot import *
+
 
 sys.path.append("../tools/")
 import sklearn
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import SelectKBest,chi2
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
@@ -25,6 +26,33 @@ from sklearn import preprocessing
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+target_label = 'poi'
+email_features_list = [
+    # 'email_address', # remit email address; informational label
+    'from_messages',
+    'from_poi_to_this_person',
+    'from_this_person_to_poi',
+    'shared_receipt_with_poi',
+    'to_messages',
+    ]
+financial_features_list = [
+    'bonus',
+    'deferral_payments',
+    'deferred_income',
+    'director_fees',
+    'exercised_stock_options',
+    'expenses',
+    'loan_advances',
+    'long_term_incentive',
+    'other',
+    'restricted_stock',
+    'restricted_stock_deferred',
+    'salary',
+    'total_payments',
+    'total_stock_value',
+]
+features_list = [target_label] + financial_features_list + email_features_list
+
 
 ### Task 2: Remove outliers
 df=pandas.DataFrame.from_records(list(data_dict.values()))
@@ -52,20 +80,14 @@ features = df_cleaned.drop('poi',1).as_matrix()
 min_max_scaler = preprocessing.MinMaxScaler()
 features_scaled=min_max_scaler.fit_transform(features)
 
-#### feature selection using random forest;
-from sklearn.ensemble import RandomForestClassifier
-clf=RandomForestClassifier(n_estimators=1000,random_state=456)
-clf.fit(features,labels)
 
+#### feature selection using SelectKBest;
+kbest = SelectKBest(chi2, k=10)
+features_kbest=kbest.fit_transform(features_scaled,labels)
+features_selected=[features_list[i+1] for i in kbest.get_support(indices=True)]
 
-important_features = []
-for x,i in enumerate(clf.feature_importances_):
-    if i>numpy.average(clf.feature_importances_):
-        important_features.append((x))
-        
 from sklearn.model_selection import train_test_split
-features_selected=features[:,important_features]
-features_train, features_test, labels_train, labels_test = train_test_split(features_selected,labels,test_size=0.3,random_state=123)
+features_train, features_test, labels_train, labels_test = train_test_split(features_scaled,labels,test_size=0.3,random_state=42)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -74,31 +96,31 @@ features_train, features_test, labels_train, labels_test = train_test_split(feat
 ### http://scikit-learn.org/stable/modules/pipeline.html
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+g_clf = GaussianNB()
+g_clf.fit(features_train,labels_train)
+pred=g_clf.predict(features_test)
 accuracy = accuracy_score(pred,labels_test)
 print "The accuracy score of Naive Bayes classifier is: %s" % accuracy
 
 
 from sklearn.ensemble import RandomForestClassifier
-clf=RandomForestClassifier()
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+r_clf=RandomForestClassifier()
+r_clf.fit(features_train,labels_train)
+pred=r_clf.predict(features_test)
 accuracy = accuracy_score(pred,labels_test)
 print "The accuracy score of Random Forest classifier is: %s" % accuracy
 
 from sklearn.ensemble import AdaBoostClassifier
-clf=AdaBoostClassifier()
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+a_clf=AdaBoostClassifier()
+a_clf.fit(features_train,labels_train)
+pred=a_clf.predict(features_test)
 accuracy = accuracy_score(pred,labels_test)
 print "The accuracy score of AdaBoost classifier is: %s" % accuracy
 
 from sklearn.linear_model import LogisticRegression
-clf=LogisticRegression()
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+l_clf=LogisticRegression()
+l_clf.fit(features_train,labels_train)
+pred=l_clf.predict(features_test)
 accuracy = accuracy_score(pred,labels_test)
 print "The accuracy score of Logistic Regression classifier is: %s" % accuracy
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
@@ -109,9 +131,10 @@ print "The accuracy score of Logistic Regression classifier is: %s" % accuracy
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-clf= GaussianNB()
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+
+r_clf=RandomForestClassifier(n_estimators=8,min_samples_split=3,max_features=7)
+r_clf.fit(features_train,labels_train)
+pred=r_clf.predict(features_test)
 
 accuracy = accuracy_score(pred,labels_test)
 recall=recall_score(labels_test,pred)
@@ -120,10 +143,9 @@ print "The accuracy score of this classifier is: %s" % accuracy
 print "The recall score of this classifier is: %s" % recall
 print "The precision score of this classifier is: %s" % precision
 
-clf=RandomForestClassifier(n_estimators=30,min_samples_split=10,max_features=8)
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
-
+clf=AdaBoostClassifier(n_estimators=5,learning_rate=1.1,algorithm='SAMME')
+a_clf.fit(features_train,labels_train)
+pred=a_clf.predict(features_test)
 accuracy = accuracy_score(pred,labels_test)
 recall=recall_score(labels_test,pred)
 precision=precision_score(labels_test,pred)
@@ -131,34 +153,21 @@ print "The accuracy score of this classifier is: %s" % accuracy
 print "The recall score of this classifier is: %s" % recall
 print "The precision score of this classifier is: %s" % precision
 
-clf=AdaBoostClassifier(n_estimators=10,learning_rate=1.5,algorithm='SAMME')
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
-accuracy = accuracy_score(pred,labels_test)
-recall=recall_score(labels_test,pred)
-precision=precision_score(labels_test,pred)
-print "The accuracy score of this classifier is: %s" % accuracy
-print "The recall score of this classifier is: %s" % recall
-print "The precision score of this classifier is: %s" % precision
 
-clf=LogisticRegression(penalty='l1' )
-clf.fit(features_train,labels_train)
-pred=clf.predict(features_test)
+#data for grading
+my_dataset=data_dict
+my_features_list=[target_label]+features_selected
+data = featureFormat(my_dataset, my_features_list)
+labels, features = targetFeatureSplit(data)
 
-accuracy = accuracy_score(pred,labels_test)
-recall=recall_score(labels_test,pred)
-precision=precision_score(labels_test,pred)
-print "The accuracy score of this classifier is: %s" % accuracy
-print "The recall score of this classifier is: %s" % recall
-print "The precision score of this classifier is: %s" % precision
-
+clf=r_clf
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-clf=RandomForestClassifier(n_estimators=30,min_samples_split=10,max_features=8)
-clf.fit(features_train,labels_train)
+
+    
 pickle.dump(clf, open("my_classifier.pkl", "w") )
 pickle.dump(df_cleaned, open("my_dataset.pkl", "w") )
-pickle.dump(features_selected, open("my_feature_list.pkl", "w") )
+pickle.dump(my_features_list, open("my_feature_list.pkl", "w") )
