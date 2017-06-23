8af0d1530cc7ab94287e689144367578063ca13c
karenlqy <qingyuli422@gmail.com>
2017-06-23 15:28:15 -0500

    Add files via upload

--- P5-Machine Learning with Enron Dataset/poi_id.py
+++ P5-Machine Learning with Enron Dataset/poi_id.py
@@ -12,13 +12,19 @@ from ggplot import *
 
 sys.path.append("../tools/")
 import sklearn
-from sklearn.feature_selection import SelectKBest,chi2
+from sklearn.feature_selection import SelectKBest
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from feature_format import featureFormat, targetFeatureSplit
-from sklearn import preprocessing
-from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -93,34 +99,49 @@ features_list.append('fraction_from_poi')
 features_list.append('fraction_to_poi')
 #features_list.append('fraction_shared')
 
-features_list.remove('from_poi_to_this_person')
-features_list.remove('from_this_person_to_poi')
-features_list.remove('from_messages')
-features_list.remove('to_messages')
+#features_list.remove('from_poi_to_this_person')
+#features_list.remove('from_this_person_to_poi')
+#features_list.remove('from_messages')
+#features_list.remove('to_messages')
 #features_list.remove('shared_receipt_with_poi')
-df_imputed=df_imputed.drop('from_poi_to_this_person',1)
-df_imputed=df_imputed.drop('from_this_person_to_poi',1)
-df_imputed=df_imputed.drop('to_messages',1)
-df_imputed=df_imputed.drop('from_messages',1)
+#df_imputed=df_imputed.drop('from_poi_to_this_person',1)
+#df_imputed=df_imputed.drop('from_this_person_to_poi',1)
+#df_imputed=df_imputed.drop('to_messages',1)
+#df_imputed=df_imputed.drop('from_messages',1)
 #df_imputed=df_imputed.drop('shared_receipt_with_poi',1)
 
 #Rescale data
 labels = df_imputed['poi'].astype(int).as_matrix()
 features = df_imputed.drop('poi',1).as_matrix()
 
-min_max_scaler = preprocessing.MinMaxScaler()
-features_scaled=min_max_scaler.fit_transform(features)
-
 
 #### feature selection using SelectKBest;
-kbest = SelectKBest(chi2, k=10)
-features_kbest=kbest.fit_transform(features_scaled,labels)
-features_selected=[features_list[i+1] for i in kbest.get_support(indices=True)]
+pipe=Pipeline([("scaler",MinMaxScaler()),
+               ("skb",SelectKBest()),
+               ("clf",RandomForestClassifier(random_state=42))])
 
-feature_scores = ['%.2f' % elem for elem in kbest.scores_ ]
-features_scores_selected=[feature_scores[i]for i in kbest.get_support(indices=True)]
-print 'Selected Features', features_selected
-print 'Feature Scores', features_scores_selected
+sss = StratifiedShuffleSplit(random_state = 0)
+parameters={'skb__k':range(1,len(features_list)),
+            "clf__n_estimators":[5,10,15,20,50,100],
+            "clf__min_samples_split":[2,3,5],
+            "clf__min_samples_leaf":[2,3,5]}
+gs=GridSearchCV(pipe,parameters,cv=sss,scoring="f1")
+gs.fit(features,labels)
+
+K_best = gs.best_estimator_.named_steps['skb']
+
+# Get SelectKBest scores, rounded to 2 decimal places, name them "feature_scores"
+feature_scores = ['%.2f' % elem for elem in K_best.scores_ ]
+# Get SelectKBest pvalues, rounded to 3 decimal places, name them "feature_scores_pvalues"
+feature_scores_pvalues = ['%.3f' % elem for elem in  K_best.pvalues_ ]
+# Get SelectKBest feature names, whose indices are stored in 'K_best.get_support',
+# create a tuple of feature names, scores and pvalues, name it "features_selected_tuple"
+features_selected_tuple=[(features_list[i+1], feature_scores[i], feature_scores_pvalues[i]) for i in K_best.get_support(indices=True)]
+features_selected=[features_list[i+1] for i in K_best.get_support(indices=True)]
+
+# Sort the tuple by score, in reverse order
+features_selected_tuple = sorted(features_selected_tuple, key=lambda feature: float(feature[1]) , reverse=True)
+print features_selected_tuple
 
 #data for grading
 #remove features that already been deleted in the dataset
@@ -131,10 +152,21 @@ print my_features_list
 data = featureFormat(my_dataset, my_features_list)
 labels, features = targetFeatureSplit(data)
 
+features_kbest=K_best.fit_transform(features,labels)
 
-from sklearn.model_selection import train_test_split
-features_train, features_test, labels_train, labels_test = train_test_split(features_scaled,labels,test_size=0.3,random_state=42)
-
+sss = StratifiedShuffleSplit(n_splits=3,test_size=0.3,random_state=42)
+for train_index,test_index in sss.split(features_kbest,labels):
+    features_train = []
+    features_test  = []
+    labels_train   = []
+    labels_test    = []
+    for ii in train_index:
+        features_train.append( features_kbest[ii] )
+        labels_train.append( labels[ii] )
+    for jj in test_index:
+        features_test.append( features_kbest[jj] )
+        labels_test.append( labels[jj] )
+        
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
@@ -193,25 +225,38 @@ print "The precision score of this classifier is: %s" % precision
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
+#parameters={"penalty":('l1','l2'),
+#            "C":(0.2,0.5,0.8,1,1.2,1.5,1.7,1.9,2),
+#            'max_iter':[100,200,500,1000]}
+
+#l_clf=LogisticRegression()
+#gs=GridSearchCV(l_clf,parameters)
+#gs.fit(features_train,labels_train)
+#l_y_pred=gs.predict(features_test)
+
+#parameters={
+#            "n_estimators":[5,10,15,20,50,100],
+#            "criterion":("gini","entropy"),
+#            "min_samples_split":[2,3,5],
+#            "min_samples_leaf":[2,3,5]}
+#r_clf=RandomForestClassifier()
+#gs=GridSearchCV(r_clf,parameters)
+#gs.fit(features_train,labels_train)
+#r_y_pred=gs.predict(features_test)
+
+#parameters={
+#            "n_neighbors":range(1,10),
+#            "algorithm":("ball_tree","kd_tree","brute"),
+#            'weights': ['uniform', 'distance'],
+#            'p': [1, 2], 'leaf_size': [1, 5, 9, 10, 20, 30, 40, 50, 60]}
+
+#k_clf=KNeighborsClassifier()
+#gs=GridSearchCV(k_clf,parameters)
+#gs.fit(features_train,labels_train)
+#k_y_pred=gs.predict(features_test)
 
 
-a_clf=AdaBoostClassifier()
-parameters={'n_estimators':[5,10,20,50,60,70,80,90,100],'learning_rate':[0.5,1,1.2,1.3,1.4,1.5,2 ],'algorithm':('SAMME','SAMME.R')}
-gs=GridSearchCV(a_clf,parameters)
-gs.fit(features_train,labels_train)
-a_y_pred=gs.predict(features_test)
-
-
-#accuracy = accuracy_score(r_y_pred,labels_test)
-#recall=recall_score(labels_test,r_y_pred)
-#precision=precision_score(labels_test,r_y_pred)
-#print "The accuracy score of this classifier is: %s" % accuracy
-#print "The recall score of this classifier is: %s" % recall
-#print "The precision score of this classifier is: %s" % precision
-
-
-
-clf=gs.best_estimator_
+clf=g_clf
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
